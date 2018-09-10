1c6bae3d1322b713ed977e538f9b8f82338b65b7
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-10 20:42:08 +0900

    update from master, resolve conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-<<<<<<< HEAD
-=======
     @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:nickname, :avatar, :background_image)
->>>>>>> master
   end
 end
