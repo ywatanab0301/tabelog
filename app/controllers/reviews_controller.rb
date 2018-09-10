82fe920148ed2085e4589a7982acd718dbc81e71
Taiki <t.chinju0909@gmail.com>
2018-09-10 20:35:15 +0900

    Create reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    # @review = Review.new(review_params)
+  end
+
+  # private
+
+  # def review_params
+  #   params.permit()
+  # end
+
 end
