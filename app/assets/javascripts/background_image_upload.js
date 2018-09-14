c3d466140f8e37acaf4560bdb22dcd88ae27bb33
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-14 21:56:11 +0900

    Upload background image

--- app/assets/javascripts/background_image_upload.js
+++ app/assets/javascripts/background_image_upload.js
@@ -1,12 +1,18 @@
 // カバー写真変更ボタンの表示
-// $(function() {
-//   $('#cover-image').on("mouseover", function(){
-//     $('.change-header-cover').show();
-//   });
-//   $('#cover-image').on("mouseout", function(){
-//     $('.change-header-cover').hide();
-//   });
-// });
+$(function() {
+  $('#cover-image').on("mouseover", function(){
+    $('.change-header-cover').show();
+  });
+  $('#cover-image').on("mouseout", function(){
+    $('.change-header-cover').hide();
+  });
+  $('.change-header-cover').change(function(){
+    console.log(this)
+    $('.cover-image-submit').show();
+  });
+});
+
+
 
 // $(function() {
 //   $(document).on("click", ".change-header-cover", function() {
@@ -17,27 +23,27 @@
 //   });
 // })
 
-// $(function() {
-//   $('.change-header-cover-list').after('<span></span>');
+$(function() {
+  $('.change-header-cover-list').after('<span></span>');
 
-//   // アップロードするファイルを選択
-//   $('.change-header-cover-list').change(function(e) {
-//     var file = $(this).prop('files')[0];
+  // アップロードするファイルを選択
+  $('.change-header-cover-list').change(function(e) {
+    var file = $(this).prop('files')[0];
 
-//     // 画像以外は処理を停止
-//     if (! file.type.match('image.*')) {
-//       // クリア
-//       $(this).val('');
-//       $('span').html('');
-//       return;
-//     }
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('span').html('');
+      return;
+    }
 
-//     // 画像表示
-//     var reader = new FileReader();
-//     reader.onload = function() {
-//       var img_src = $('<img class= cover-image, width= >').attr('src', reader.result);
-//       $(e.target).next().html(img_src);
-//     }
-//     reader.readAsDataURL(file);
-//   });
-// });
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+      var img_src = $('<img class= cover-image, width= >').attr('src', reader.result);
+      $(e.target).next().html(img_src);
+    }
+    reader.readAsDataURL(file);
+  });
+});
