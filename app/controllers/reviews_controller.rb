d50fbdec783582b2df3b73af625b426eac06a69a
Taiki <t.chinju0909@gmail.com>
2018-09-15 18:45:36 +0900

    Definite new and create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,21 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @image =  Image.new
+    @review.images.build
   end
 
   def create
+    Review.new(review_params)
   end
 
+
+  private
+    def review_params
+      params.require(:review).permit(:lunch_dinner, :rate, :food_rate, :cp_rate, :service_rate, :drink_rate, :atmosphere, :title, :text, :visit_day, images_attributes: [:pic1, :pic2, :pic3, :pic4, :pic5]).merge(user_id: current_user.id)
+    end
+
+    # def image_params
+    #   params.permit(:pic1, :pic2, :pic3, :pic4, :pic5)
+    # end
 end
