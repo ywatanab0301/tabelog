efbad4c321e9b8c992d640db292b6b94774ce705
Taiki <t.chinju0909@gmail.com>
2018-09-15 18:47:19 +0900

    Edit review routing nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shops
-  resources :users
-  resources :reviews
-#usersとreviewsはネストさせる
-  # resources :users do
-  #   resources :reviews
-  # end
+  resources :users do
+    resources :reviews
+  end
 end
