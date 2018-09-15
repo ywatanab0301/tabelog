2106af9a133b7f2338bd1de10d7b7989e343dac4
Taiki <t.chinju0909@gmail.com>
2018-09-15 13:49:20 +0900

    Edit review association

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
-
+  belongs_to :user
+  has_many :likes
+  has_many :images
+  belongs_to :shop
+  has_many :review_tags
+  has_many :tags, through: :review_tags
+  has_many :review_budgets
+  has_many :budgets, through: :review_budgets
 end
