4aec3c6b9a92b7c0859256bf5ed6e0f111d1f117
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-15 18:12:51 +0900

    change my page view slightly (fix bugs)

--- app/assets/javascripts/background_image_upload.js
+++ app/assets/javascripts/background_image_upload.js
@@ -11,39 +11,3 @@ $(function() {
     $('.cover-image-submit').show();
   });
 });
-
-
-
-// $(function() {
-//   $(document).on("click", ".change-header-cover", function() {
-//     $('.change-header-cover-list').slideDown('fast');
-//   });
-//   $(document).on("mouseout", ".change-header-cover-list", function() {
-//     $('.change-header-cover-list').hide();
-//   });
-// })
-
-$(function() {
-  $('.change-header-cover-list').after('<span></span>');
-
-  // アップロードするファイルを選択
-  $('.change-header-cover-list').change(function(e) {
-    var file = $(this).prop('files')[0];
-
-    // 画像以外は処理を停止
-    if (! file.type.match('image.*')) {
-      // クリア
-      $(this).val('');
-      $('span').html('');
-      return;
-    }
-
-    // 画像表示
-    var reader = new FileReader();
-    reader.onload = function() {
-      var img_src = $('<img class= cover-image, width= >').attr('src', reader.result);
-      $(e.target).next().html(img_src);
-    }
-    reader.readAsDataURL(file);
-  });
-});
