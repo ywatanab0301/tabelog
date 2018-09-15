44742d763502a9134aa5f614d2dbc46edd29acde
shoji_naoto <onyxnaoto@gmail.com>
2018-09-15 20:12:04 +0900

    move-show
    showアクションの位置を変更

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -12,13 +12,13 @@ class ShopsController < ApplicationController
     Shop.create(shop_params)
   end
 
+  def show
+  end
+
   private
 
   def shop_params
     params.require(:shop).permit(:shop_name, :shop_name_f, :phone_number, :city_address, :building, :hours, :closed_day, :prefecture_ids, :genre_ids)
-  end
-
-  def show
   end
 
 end
