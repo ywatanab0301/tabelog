ceb08c2dadfc8cc9f19951ed76bc454720922486
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-17 17:45:30 +0900

    Set routes for shops/show/menu and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shops
+  get 'shops/show/menu' => 'shops#show_menu'
+  get 'shops/show/reviews' => 'shops#show_reviews'
   resources :users do
     resources :reviews
   end
