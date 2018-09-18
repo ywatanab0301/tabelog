9bc770b90abf81f9f4c7fba02279b967aefe2fb1
Taiki <t.chinju0909@gmail.com>
2018-09-18 21:31:36 +0900

    Routing likes :create and :destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :shops
   resources :searches
-  resources :users do
-    resources :reviews, only: [:new, :create]
+  resources :users, shallow:true do
+    resources :reviews, only: [:new, :create, :show] do
+      resources :likes, only: [:create, :destroy]
+    end
   end
+
+  # post   '/like/:review_id' => 'likes#like',   as: 'like'
+  # delete '/like/:review_id' => 'likes#unlike', as: 'unlike'
+
 end
