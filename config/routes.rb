decdc766c34b3eb1eed5396430dfe80f0d7ecfff
shoji_naoto <onyxnaoto@gmail.com>
2018-09-19 17:10:52 +0900

    edit-routes
    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   resources :shops
   resources :searches
   resources :users do
-    member do
-      get :following, :followers
-    end
     resources :reviews, only: [:new, :create]
   end
-  resources :relationships, only:[:create, :destroy]
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 end
