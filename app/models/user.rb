ee3280dd9b0c3d89b0b8a30e8be32956139f396e
shoji_naoto <onyxnaoto@gmail.com>
2018-09-19 10:05:32 +0900

    create-relationship
    UserとRelationshipの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,23 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :like_reviews, through: :likes, source: :review
   has_many :likes, dependent: :destroy
-  has_many :relationships
-  # こちらが能動的関係
-  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
-  # こちらが受動的関係
-  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
-  has_many :followings, through: :following_relationships
-  has_many :followers, through: :follower_relationships
   has_many :reservations
+  has_many :relationships
+  # こちらが能動的関係、フォローしているユーザーを取り出す
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  # こちらが受動的関係、、フォローしているユーザーを取り出す
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+    def following?(other_user)
+      following_relationships.find_by(following_id: other_usr.id)
+    end
+
+    def follow!(other_user)
+      following_relationships.create!(following_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+      following_relationships.find_by(following_id: other_user.id).destroy
+    end
 end
