f65fe0f9e968db9b301bc86ffbc69f96c3709e98
Taiki <t.chinju0909@gmail.com>
2018-09-20 16:28:29 +0900

    Delete double mig files

--- db/migrate/20180918102142_add_pic4_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddPic4ToReviews < ActiveRecord::Migration[5.0]
-  def change
-    add_column :reviews, :pic4, :string
-  end
-end
--- db/migrate/20180918103254_remove_share_with_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveShareWithFromReviews < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :reviews, :share_with, :integer
-  end
-end
