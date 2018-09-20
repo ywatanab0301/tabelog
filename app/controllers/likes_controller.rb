cd50eae9a3bd173b12672f67528b315c18c9d3b0
Taiki <t.chinju0909@gmail.com>
2018-09-20 16:46:24 +0900

    Definite like's action in like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,34 @@
 class LikesController < ApplicationController
-  def create
-    @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
-    @likes = Like.where(review_id: params[:review_id])
-    @reviews = review.all
+  before_action :set_variables
+
+  def like
+    like = current_user.likes.new(review_id: @review.id)
+    like.save
   end
 
-  def destroy
-    like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
+  def unlike
+    like = current_user.likes.find_by(review_id: @review.id)
     like.destroy
-    @likes = Like.where(review_id: params[:review_id])
-    @reviews = review.all
   end
+
+  private
+
+  def set_variables
+    @review = Review.find(params[:review_id])
+    @id_name = "#like-link-#{@review.id}"
+    @id_heart = "#heart-#{@review.id}"
+  end
+
+  # def create
+  #   @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
+  #   @likes = Like.where(review_id: params[:review_id])
+  #   @reviews = review.all
+  # end
+
+  # def destroy
+  #   like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
+  #   like.destroy
+  #   @likes = Like.where(review_id: params[:review_id])
+  #   @reviews = review.all
+  # end
 end
