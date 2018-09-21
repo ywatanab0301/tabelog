107a7c4dda46162f2534b07e78a10de01ec5a52f
Taiki <t.chinju0909@gmail.com>
2018-09-21 18:24:41 +0900

    Revise how to write

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,11 @@ class LikesController < ApplicationController
   before_action :set_variables
 
   def like
-    like = current_user.likes.new(review_id: @review.id)
-    like.save
+    like = current_user.likes.create(review_id: @review.id)
   end
 
   def unlike
-    like = current_user.likes.find_by(review_id: @review.id)
-    like.destroy
+    like = current_user.likes.find_by(review_id: @review.id).destroy
   end
 
   private
