2a4109e83fef08864d20b0d8e2de6bb0c6136922
牧野友里菜 <yurinamakino@gmail.com>
2018-09-23 22:18:31 +0900

    吹き出し作成

---
+++ app/assets/javascripts/shop_index_search.js
@@ -0,0 +1,16 @@
+$(function() {
+  $(".content__side__top__area__main").hover(function(){
+    $(this).next('p').show();
+  }, function(){
+    $(this).next('p').hide();
+  });
+});
+
+$(function() {
+  $(".content__side__top__genre__main").hover(function(){
+    $(this).next('p').show();
+  }, function(){
+    $(this).next('p').hide();
+  });
+});
+
