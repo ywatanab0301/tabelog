4118335cacb22336fdf540dec13254d4d819d496
牧野友里菜 <yurinamakino@gmail.com>
2018-09-24 21:58:25 +0900

    jquelyで表示

--- app/assets/javascripts/shop_index_search.js
+++ app/assets/javascripts/shop_index_search.js
@@ -1,16 +1,16 @@
 $(function() {
   $(".content__side__top__area__main").hover(function(){
-    $(this).next('p').show();
-  }, function(){
-    $(this).next('p').hide();
+    $(this).next('.content__side__top__area__box').show();
+  // }, function(){
+  //   $(this).next('.content__side__top__area__box').hide();
   });
 });
 
 $(function() {
   $(".content__side__top__genre__main").hover(function(){
-    $(this).next('p').show();
-  }, function(){
-    $(this).next('p').hide();
+    $(this).next('.content__side__top__genre__box').show();
+  // }, function(){
+  //   $(this).next('p').hide();
   });
 });
 
