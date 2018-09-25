e242345d27f90d7cfedf58eb6d1973b475d98d8b
牧野友里菜 <yurinamakino@gmail.com>
2018-09-25 18:30:48 +0900

    sideのviewを整える

--- app/assets/javascripts/shop_index_search.js
+++ app/assets/javascripts/shop_index_search.js
@@ -1,16 +1,20 @@
 $(function() {
   $(".content__side__top__area__main").hover(function(){
-    $(this).next('.content__side__top__area__box').show();
-  // }, function(){
-  //   $(this).next('.content__side__top__area__box').hide();
+    $(this).next('.content__side__top__area__box', '').show();
+  }, function(){
+    $(".content__main").hover(function(){
+      $('.content__side__top__area__box').hide();
+    })
   });
 });
 
 $(function() {
   $(".content__side__top__genre__main").hover(function(){
     $(this).next('.content__side__top__genre__box').show();
-  // }, function(){
-  //   $(this).next('p').hide();
+  }, function(){
+    $(".content__main").hover(function(){
+      $('.content__side__top__genre__box').hide();
+    })
   });
 });
 
