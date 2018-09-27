65e42e9a0a09e9030a0c6749454daef6dcd80013
信夫知也 <kazooie.nobuo@gmail.com>
2018-09-27 10:48:21 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :searches
   resources :shops do
-    get 'top_page', on: :collection
+    get 'top_page'
     get 'search_result', on: :collection
     resources :reviews, only: [:new, :create, :edit, :update, :destroy, :show]
     get 'menu' => 'shops#show_menu'
