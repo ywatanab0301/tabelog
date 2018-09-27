8a056d7252c10af5edc93e2c3c4484037354298d
信夫知也 <kazooie.nobuo@gmail.com>
2018-09-27 14:15:49 +0900

    Add set to get_shop_info

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,5 @@
 class ShopsController < ApplicationController
-  before_action :get_shop_info, only: [:show_menu, :show_reviews, :sort_popular, :sort_visit]
+  before_action :set_get_shop_info, only: [:show_menu, :show_reviews, :sort_popular, :sort_visit]
 
   def index
     if params[:search].present?
@@ -101,7 +101,7 @@ class ShopsController < ApplicationController
       :station)
   end
 
-  def get_shop_info
+  def set_get_shop_info
     @shop = Shop.find(params[:shop_id])
     @prefecture = @shop.prefectures
     @genre = @shop.genres
