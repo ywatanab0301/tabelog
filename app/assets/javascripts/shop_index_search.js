0adef21eafdf09bf55624e994e64cacd460b8d8d
牧野友里菜 <yurinamakino@gmail.com>
2018-09-27 19:16:38 +0900

    navのCSSをjquelyで整える

--- app/assets/javascripts/shop_index_search.js
+++ app/assets/javascripts/shop_index_search.js
@@ -1,3 +1,4 @@
+// 選択肢を出す
 $(function() {
   $(".content__side__top__area__main").hover(function(){
     $(this).next('.content__side__top__area__box').show();
@@ -18,17 +19,48 @@ $(function() {
   });
 });
 
+// タブの色変更
 $(function() {
-  $("li.nav-item.rank").on('mouseover', function(){
-    $('li.nav-item.rank').css('borderTopColor', '#fcb720');
-    $('a.nav-link.rank').css('color', '#fcb720');
-  });
-  $("li.nav-item.rank").on('mouseout', function(){
-    $('li.nav-item.rank').css('borderTopColor', '');
-    $('a.nav-link.rank').css('color', '');
+  $("li.nav-item.origin").on('click mouseover', function(){
+    $(this).css('borderTopColor', '#e06650');
+    $('li.nav-item.origin span').css('color', '#e06650');
+    $('li.nav-item.origin .fa.fa-utensils').css('color', '#e06650');
   });
 });
 
+$(function() {
+  $("li.nav-item.rank").on('click', function(){
+    $(this).css('background-color', '#fff');
+    $("li.nav-item.review").css('background-color', '');
+    $("li.nav-item.review").css('borderTopColor', '');
+    $('a.nav-link.review').css('color', '');
+    $("li.nav-item.origin").css('background-color', '#faf8f5');
+    $('li.nav-item.origin').css('borderTopColor', '#e0dccc');
+    $('li.nav-item.origin span').css('color', '#000');
+    $('li.nav-item.origin .fa.fa-utensils').css('color', '#000');
+  });
+  $("li.nav-item.rank").on('click mouseover', function(){
+    $(this).css('borderTopColor', '#fcb720');
+    $('a.nav-link.rank').css('color', '#fcb720');
+  });
+});
+
+$(function() {
+  $("li.nav-item.review").on('click', function(){
+    $(this).css('background-color', '#fff');
+    $("li.nav-item.rank").css('background-color', '');
+    $("li.nav-item.rank").css('borderTopColor', '');
+    $('a.nav-link.rank').css('color', '');
+    $('li.nav-item.origin span').css('color', '#000');
+    $('li.nav-item.origin .fa.fa-utensils').css('color', '#000');
+  });
+  $("li.nav-item.review").on('click mouseover', function(){
+    $(this).css('borderTopColor', '#88bd2a');
+    $('a.nav-link.review').css('color', '#88bd2a');
+  });
+});
+
+// 並び替え
 $(function(){
   $("li.nav-item.rank").on('click', function(){
     $('.content__main__shop').html(
@@ -49,17 +81,7 @@ $(function(){
   });
 });
 
-$(function() {
-  $("li.nav-item.review").on('mouseover', function(){
-    $('li.nav-item.review').css('borderTopColor', '#88bd2a');
-    $('a.nav-link.review').css('color', '#88bd2a');
-  });
-  $("li.nav-item.review").on('mouseout', function(){
-    $('li.nav-item.review').css('borderTopColor', '');
-    $('a.nav-link.review').css('color', '');
-  });
-});
-
+// 写真の変更
 $(function(){
  $('ul.photolist img').hover(function(){
   var $thisImg = $(this).attr('src');
