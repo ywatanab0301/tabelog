38fbc0e2f79ccb489670698d5e268ce9f7d0fb75
shoji_naoto <onyxnaoto@gmail.com>
2018-09-29 14:24:29 +0900

    0929-edit
    drop-down-menuの作成とその他sassの微調整

--- app/assets/javascripts/drop_down_menu.js
+++ app/assets/javascripts/drop_down_menu.js
@@ -1,19 +1,41 @@
 $(function(){
-  $(".fas.fa-sort-down").click(function(){
-    $(".notification-sort__box").hide();
-    $(".notification-sort__box",this).slideDown("fast");
-  },function(){
-    console.log("over！！！！");
-    $(".notification-sort__box",this).slideUp();
-  });
+  $('.fas.fa-sort-down').click(function(){
+    $('#menu',this).stop(true,true).slideToggle();
+    });
+  // $('.fas.fa-sort-down').click(function(){
+  //   $('#menu',this).stop(true,true).slideUp();
+  // });
 });
 
+
 $(function(){
-  $(".fas.fa-sort-down").click(function(){
-    $(".setting-list").hide();
-    $(".setting-list",this).slideDown("fast");
-  },function(){
-    console.log("over！！！！");
-    $(".setting-list",this).slideUp();
-  });
+  function buildHTML(list){
+  var list = '';
+
+  var html = `<div class='balloon'>
+                  <ul class='setting-list'>
+                    <li class='setting-list__item'>
+                      <a class='setting-list__target' href='/following/#{current_user.id}'>フォロー中一覧</a>
+                    </li>
+                    <li class='setting-list__item'>
+                      <a class='setting-list__target' href='#'>下書き一覧</a>
+                    </li>
+                    <li class='setting-list__item'>
+                      <a class='setting-list__target' href='#'>設定</a>
+                    </li>
+                    <li class='setting-list__item'>
+                      <a class='setting-list__target' href='/users/#{current_user.id}'>ログアウト</a>
+                    </li>
+                  </ul>
+                </div>`
+  }
+  list = html
+  if list == '';
+  $('.fas.fa-sort-down').click(function(){
+    $('#menu').append(html);
+    });
+  else list == html;
+  $('.fas.fa-sort-down').click(function(){
+    $('#menu').empty(html);
+    });
 });
