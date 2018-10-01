aee8804f5ae4982072a0087a9e40e26b26f71c3c
信夫知也 <kazooie.nobuo@gmail.com>
2018-10-02 00:03:08 +0900

    Add star & moon &  star to want index

--- app/models/want.rb
+++ app/models/want.rb
@@ -3,4 +3,12 @@ class Want < ApplicationRecord
 
   belongs_to :user, counter_cache: :wants_count
   belongs_to :shop
+
+  def star_review_average
+    reviews.average(:rate).round if reviews.present?
+  end
+
+  def review_average
+    reviews.average(:rate).round(2) if reviews.present?
+  end
 end
