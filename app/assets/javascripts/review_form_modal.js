3def424c18773005005f6bbf960f314c9ecf25d6
Taiki <t.chinju0909@gmail.com>
2018-10-03 15:57:02 +0900

    Small change in view

--- app/assets/javascripts/review_form_modal.js
+++ app/assets/javascripts/review_form_modal.js
@@ -1,18 +1,17 @@
 $(function() {
-  // 「Open」ボタンをクリック時に、fadeInメソッドでHTML要素を表示する
+
+
   $('.postbtn-link').on('click', function() {
     $('.overlay, .modalWindow').fadeIn();
   });
 
-  // 「Close」ボタンをクリック時に、fadeOutメソッドでHTML要素を非表示にする
   $('.close').on('click', function() {
     $('.overlay, .modalWindow').fadeOut();
   });
 
-  locateCenter();  // => モーダルウィンドウを中央配置するための初期値を設定する
-  $(window).resize(locateCenter);  // => ウィンドウのリサイズに合わせて、モーダルウィンドウの配置を変える
+  locateCenter();
+  $(window).resize(locateCenter);
 
-// モーダルウィンドウを中央配置するための配置場所を計算する関数
   function locateCenter() {
     let w = $(window).width();
     let h = $(window).height();
