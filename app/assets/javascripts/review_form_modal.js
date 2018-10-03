d113f89b5288c4e743c353527d55b89f6728fd77
信夫知也 <kazooie.nobuo@gmail.com>
2018-10-04 02:53:57 +0900

    Add modal

--- app/assets/javascripts/review_form_modal.js
+++ app/assets/javascripts/review_form_modal.js
@@ -1,11 +1,10 @@
 $(function() {
 
-
   $('.postbtn-link').on('click', function() {
     $('.overlay, .modalWindow').fadeIn();
   });
 
-  $('.close').on('click', function() {
+  $('.overlay').on('click', function() {
     $('.overlay, .modalWindow').fadeOut();
   });
 
@@ -25,3 +24,192 @@ $(function() {
     });
   }
 });
+
+$(function() {
+
+  $('.fas.fa-heart').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+$(function() {
+
+  $('.postsbtn-link').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+$(function() {
+
+  $('.link-login').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+$(function() {
+
+  $('.user-menu__text').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+$(function() {
+
+  $('.link-logins').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+$(function() {
+
+  $('.user-menu__target').on('click', function() {
+    $('.overlay, .modalWindow').fadeIn();
+  });
+
+  $('.overlay').on('click', function() {
+    $('.overlay, .modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.modalWindow').outerWidth();
+    let ch = $('.modalWindow').outerHeight();
+
+    $('.modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
+
+// $(function() {
+
+//   $('.fa.fa-heart').on('click', function() {
+//     $('.overlay, .modalWindow').fadeIn();
+//   });
+
+//   $('.overlay').on('click', function() {
+//     $('.overlay, .modalWindow').fadeOut();
+//   });
+
+//   locateCenter();
+//   $(window).resize(locateCenter);
+
+//   function locateCenter() {
+//     let w = $(window).width();
+//     let h = $(window).height();
+
+//     let cw = $('.modalWindow').outerWidth();
+//     let ch = $('.modalWindow').outerHeight();
+
+//     $('.modalWindow').css({
+//       'left': ((w - cw) / 2) + 'px',
+//       'top': ((h - ch) / 2) + 'px'
+//     });
+//   }
+// });
