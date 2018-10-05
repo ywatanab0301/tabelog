b08d5073755c02b1a82138b5aa87351b7ca9f59b
Yuta Watanabe <ywatanab0301@gmail.com>
2018-10-05 18:57:43 +0900

    Implement basic authentication for production environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :basic_auth, :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar, :background_image])
   end
 
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == 'tabelog34'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
