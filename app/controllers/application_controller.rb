e66572392df8ff15bf3e5a4358561dffc84b9a30
Yuta Watanabe <ywatanab0301@gmail.com>
2018-10-05 15:43:32 +0900

    Implement basic_authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar, :background_image])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'tabelog34'
+    end
   end
 end
